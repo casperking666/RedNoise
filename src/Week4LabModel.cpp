9093d1206873eaf4c44be43b40e9abcef6b0264c
Casper Wang <casperkingwang@gmail.com>
2022-10-23 12:00:51 +0100

    Week 4, part 3 done

--- src/Week4LabModel.cpp
+++ src/Week4LabModel.cpp
@@ -9,32 +9,61 @@
 #include <TextureMap.h>
 #include "RedNoise.h"
 #include <ModelTriangle.h>
+#include <map>
+
 
 #define WIDTH 320
 #define HEIGHT 240
 
 
-std::vector<ModelTriangle> readFile(std::string fileName, float scale) {
+std::vector<ModelTriangle> readGeoFile(std::string fileName, float scale, std::map<std::string, Colour> palette) {
     std::string text;
     std::ifstream file(fileName);
     std::vector<glm::vec3> vertices;
     std::vector<ModelTriangle> connections;
+    std::string name;
     while (std::getline(file, text)) {
         std::vector<std::string> values = split(text, ' ');
-        if (values[0] == "v") {
+        if (values[0] == "usemtl") {
+            name = values[1];
+        } else if (values[0] == "v") {
             vertices.push_back(scale * glm::vec3 (std::stof(values[1]), std::stof(values[2]), std::stof(values[3])));
-        }
-        else if (values[0] == "f") {
+        } else if (values[0] == "f") {
             glm::vec3 vertex1 = vertices[values[1][0] - '0' - 1]; // - '0' is how you convert char to int
             glm::vec3 vertex2 = vertices[values[2][0] - '0' - 1];
             glm::vec3 vertex3 = vertices[values[3][0] - '0' - 1];
-            connections.push_back(ModelTriangle(vertex1, vertex2, vertex3, Colour()));
+            connections.push_back(ModelTriangle(vertex1, vertex2, vertex3, palette[name]));
         }
     }
     return connections;
 }
 
+std::map<std::string, Colour> readMtlFile(std::string filename) {
+    std::string text;
+    std::map<std::string, Colour> palette;
+    std::vector<std::string> names;
+    std::vector<Colour> colours;
+    std::ifstream matFile(filename);
+    while (std::getline(matFile, text)) {
+        std::vector<std::string> values = split(text, ' ');
+        if (values[0] == "newmtl") {
+            names.push_back(values[1]);
+        } else if (values[0] == "Kd") {
+            Colour tempColour = Colour(std::stof(values[1]) * 255, std::stof(values[2]) * 255, std::stof(values[3]) * 255);
+            colours.push_back(tempColour);
+        }
+    }
+    for (unsigned int i = 0; i < names.size(); i++) {
+        palette[names[i]] = colours[i];
+    }
+    return palette;
+}
 
+std::vector<ModelTriangle> readFiles(std::string geoFileName, std::string matFileName, float scale) {
+    std::map<std::string, Colour> palette = readMtlFile(matFileName);
+    std::vector<ModelTriangle> connections = readGeoFile(geoFileName, scale, palette);
+    return connections;
+}
 
 void handleEvent(SDL_Event event, DrawingWindow &window) {
     if (event.type == SDL_KEYDOWN) {
@@ -42,7 +71,7 @@ void handleEvent(SDL_Event event, DrawingWindow &window) {
         else if (event.key.keysym.sym == SDLK_RIGHT) std::cout << "RIGHT" << std::endl;
         else if (event.key.keysym.sym == SDLK_UP) std::cout << "UP" << std::endl;
         else if (event.key.keysym.sym == SDLK_DOWN) {
-            std::vector<ModelTriangle> connections = readFile("../cornell-box.obj", 1);
+            std::vector<ModelTriangle> connections = readFiles("../cornell-box.obj", "../cornell-box.mtl", 1);
             for (ModelTriangle connection : connections) {
                 std::cout << connection << std::endl;
             }

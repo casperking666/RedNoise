9e66af8f1ed8a50fdcd64e94e710197fa94459e2
Casper Wang <casperkingwang@gmail.com>
2022-10-26 21:14:05 +0100

    Week 4, part 8 done

--- src/Week4LabModel.cpp
+++ src/Week4LabModel.cpp
@@ -12,6 +12,7 @@
 #include <ModelTriangle.h>
 #include <map>
 #include <string>
+#include <tuple>
 
 
 #define WIDTH 320
@@ -23,37 +24,20 @@ uint32_t translateColor(Colour colour) {
     return colorUint;
 }
 
-void drawLine(DrawingWindow &window, CanvasPoint from, CanvasPoint to, Colour color) {
-//    window.clearPixels();
-    float xDiff = to.x - from.x;
-    float yDiff = to.y - from.y;
-    float numOfSteps = std::max(abs(xDiff), abs(yDiff));
-    float xStepSize = xDiff/numOfSteps;
-    float yStepSize = yDiff/numOfSteps;
-    for (float i = 0.0; i < numOfSteps; i++) {
-        uint32_t colorUint = translateColor(color);
-        float x = from.x + xStepSize * i;
-        float y = from.y + yStepSize * i;
-        window.setPixelColour(round(x), round(y), colorUint);
-    }
-}
-
-
-void drawTriangle(DrawingWindow &window, CanvasPoint v0, CanvasPoint v1, CanvasPoint v2, Colour color) {
-//    window.clearPixels();
+CanvasTriangle sortCanvasPoint(CanvasPoint v0, CanvasPoint v1, CanvasPoint v2) {
+    if (v1.y < v0.y) std::swap(v0, v1);
+    if (v2.y < v0.y) std::swap(v0, v2);
+    if (v2.y < v1.y) std::swap(v1, v2);
     CanvasTriangle triangle = CanvasTriangle(v0, v1, v2);
-    //std::cout << triangle;
-    drawLine(window, triangle.v0(), triangle.v1(), color);
-    drawLine(window, triangle.v0(), triangle.v2(), color);
-    drawLine(window, triangle.v1(), triangle.v2(), color);
+    return triangle;
 }
 
-void drawTriangles(DrawingWindow &window, std::vector<CanvasTriangle> triangles, Colour color) {
-    for (CanvasTriangle triangle : triangles) {
-        drawTriangle(window, triangle.v0(), triangle.v1(), triangle.v2(), color);
-    }
+CanvasPoint findPoint(CanvasPoint v0, CanvasPoint v1, CanvasPoint v2) {
+    float coorX = abs(v0.x - v2.x)/abs(v0.y - v2.y) * abs(v0.y - v1.y);
+    if (v0.x <= v2.x) coorX = coorX + v0.x;
+    else coorX = -coorX + v0.x;
+    return CanvasPoint(coorX, v1.y);
 }
-
 
 std::vector<ModelTriangle> readGeoFile(std::string fileName, float scale, std::map<std::string, Colour> palette) {
     std::string text;
@@ -69,9 +53,9 @@ std::vector<ModelTriangle> readGeoFile(std::string fileName, float scale, std::m
             vertices.push_back(scale * glm::vec3 (std::stof(values[1]), std::stof(values[2]), std::stof(values[3])));
         } else if (values[0] == "f") {
 //            glm::vec3 vertex1 = vertices[values[1][0] - '0' - 1]; // - '0' is how you convert char to int
-            glm::vec3 vertex1 = vertices[std::stoi(split(values[1], '/')[0]) - 1]; // - '0' is how you convert char to int
-            glm::vec3 vertex2 = vertices[std::stoi(split(values[2], '/')[0]) - 1]; // - '0' is how you convert char to int
-            glm::vec3 vertex3 = vertices[std::stoi(split(values[3], '/')[0]) - 1]; // - '0' is how you convert char to int
+            glm::vec3 vertex1 = vertices[std::stoi(split(values[1], '/')[0]) - 1]; // -1 as vertex starts from 1 in obj
+            glm::vec3 vertex2 = vertices[std::stoi(split(values[2], '/')[0]) - 1];
+            glm::vec3 vertex3 = vertices[std::stoi(split(values[3], '/')[0]) - 1];
             connections.push_back(ModelTriangle(vertex1, vertex2, vertex3, palette[name]));
         }
     }
@@ -105,31 +89,91 @@ std::vector<ModelTriangle> readFiles(std::string geoFileName, std::string matFil
     return connections;
 }
 
+void drawLine(DrawingWindow &window, CanvasPoint from, CanvasPoint to, Colour color) {
+//    window.clearPixels();
+    float xDiff = to.x - from.x;
+    float yDiff = to.y - from.y;
+    float numOfSteps = std::max(abs(xDiff), abs(yDiff));
+    float xStepSize = xDiff/numOfSteps;
+    float yStepSize = yDiff/numOfSteps;
+    for (float i = 0.0; i < numOfSteps; i++) {
+        uint32_t colorUint = translateColor(color);
+        float x = from.x + xStepSize * i;
+        float y = from.y + yStepSize * i;
+        window.setPixelColour(round(x), round(y), colorUint);
+    }
+}
+
+
+void drawTriangle(DrawingWindow &window, CanvasPoint v0, CanvasPoint v1, CanvasPoint v2, Colour color) {
+    CanvasTriangle triangle = CanvasTriangle(v0, v1, v2);
+    drawLine(window, triangle.v0(), triangle.v1(), color);
+    drawLine(window, triangle.v0(), triangle.v2(), color);
+    drawLine(window, triangle.v1(), triangle.v2(), color);
+}
+
+void drawFilledTriangle(DrawingWindow &window, CanvasPoint v0, CanvasPoint v1, CanvasPoint v2, Colour color) {
+    CanvasTriangle triangle = sortCanvasPoint(v0, v1, v2);
+    CanvasPoint intersect = findPoint(triangle.v0(), triangle.v1(), triangle.v2());
+    float numOfSteps0 = abs(triangle.v0().y - triangle.v1().y);
+    float numOfSteps1 = abs((triangle.v2().y - triangle.v1().y));
+
+    float xStepSize0 = (intersect.x - triangle.v0().x) / numOfSteps0;
+    float xStepSize1 = (triangle.v2().x - intersect.x) / numOfSteps1;
+    float xStepSize2 = (triangle.v1().x - triangle.v0().x) / numOfSteps0;
+    float xStepSize3 = (triangle.v2().x - triangle.v1().x) / numOfSteps1;
+
+    for (float i = 0; i < numOfSteps0; i++) {
+        CanvasPoint startCanvas = CanvasPoint(round(triangle.v0().x + xStepSize0 * i), triangle.v0().y + i);
+        CanvasPoint endCanvas = CanvasPoint(round(triangle.v0().x + xStepSize2 * i), triangle.v0().y + i);
+        drawLine(window, startCanvas, endCanvas, color);
+    }
+
+    for (float i = 0; i < numOfSteps1; i++) {
+        CanvasPoint startCanvas = CanvasPoint(round(intersect.x + xStepSize1 * i), intersect.y + i);
+        CanvasPoint endCanvas = CanvasPoint(round(triangle.v1().x + xStepSize3 * i), intersect.y + i);
+        drawLine(window, startCanvas, endCanvas, color);
+    }
+
+}
+
+void drawTriangles(DrawingWindow &window, std::vector<std::tuple<Colour, CanvasTriangle>> triangles) {
+    for (std::tuple<Colour, CanvasTriangle> triangleTuple : triangles) {
+        Colour colour;
+        CanvasTriangle triangle;
+        std::tie(colour, triangle) = triangleTuple;
+        drawFilledTriangle(window, triangle.v0(), triangle.v1(), triangle.v2(), colour);
+    }
+}
+
 CanvasPoint getCanvasIntersectionPoint(glm::vec3 cameraPosition, glm::vec3 vertexPosition, float focalLength) {
-//    float offset = -1 * cameraPosition.z;
-//    float z = offset + vertexPosition.z;
-    glm::vec3 cameraCoordinate = vertexPosition - cameraPosition;
-    float scale_u = focalLength * ((cameraCoordinate.x) / abs(cameraCoordinate.z)) * 180; // very bad practice, but will just leave it like this
-    float scale_v = -1 * focalLength * ((cameraCoordinate.y) / abs(cameraCoordinate.z)) * 180;
-    float image_u = scale_u + WIDTH / 2; // the width might be wrong
+    // this is quite confusing. firstly, we are using object coordinate, this way is a bit easier
+    // as towards the camera is regarded as the positive z direction
+    // this is still not 100% certain in terms of x and y, we will see
+    glm::vec3 objectCoordinate = vertexPosition - cameraPosition;
+    float scale_u = focalLength * ((objectCoordinate.x) / abs(objectCoordinate.z)) * 180; // very bad practice, but will just leave it like this
+    float scale_v = - 1 * focalLength * ((objectCoordinate.y) / abs(objectCoordinate.z)) * 180; // -1 is interesting too, pixel coordinate has reversed y, obj is constructed around center
+    float image_u = scale_u + WIDTH / 2;
     float image_v = scale_v + HEIGHT / 2;
     CanvasPoint coordinate = CanvasPoint(image_u, image_v);
     return coordinate;
 }
 
-std::vector<CanvasTriangle> convertModTriToTri(std::vector<ModelTriangle> connections) {
-    std::vector<CanvasTriangle> triangles;
+std::vector<std::tuple<Colour, CanvasTriangle>> convertModTriToTri(std::vector<ModelTriangle> connections) {
+    std::vector<std::tuple<Colour, CanvasTriangle>> triangles;
     std::vector<CanvasPoint> points;
+    std::vector<Colour> colours;
     glm::vec3 cameraPosition = glm::vec3(0.0, 0.0, 4.0);
     float focalLength = 2.0;
     for (ModelTriangle connection : connections) {
+        Colour colour = connection.colour;
         for (glm::vec3 point3d : connection.vertices) {
             CanvasPoint point = getCanvasIntersectionPoint(cameraPosition, point3d, focalLength);
             points.push_back(point);
         }
         CanvasTriangle triangle = CanvasTriangle(points[0], points[1], points[2]);
-        triangles.push_back(triangle);
-        points = std::vector<CanvasPoint>();
+        triangles.push_back({colour, triangle});
+        points = std::vector<CanvasPoint>(); // this is like a neat way to erase the vector
     }
     return triangles;
 }
@@ -144,7 +188,6 @@ void drawPoints(std::vector<ModelTriangle> connections, DrawingWindow &window, u
 //            std::cout << point << std::endl;
             window.setPixelColour(int(round(point.x)), int(round(point.y)), colourUnit); // could write a function for scaling or include in the existing function
         }
-
     }
 }
 
@@ -157,15 +200,12 @@ void handleEvent(SDL_Event event, DrawingWindow &window) {
             Colour colour = Colour(255, 255, 255);
             uint32_t colourUnit = translateColor(colour);
             std::vector<ModelTriangle> connections = readFiles("../cornell-box.obj", "../cornell-box.mtl", 0.35);
-            drawPoints(connections, window, colourUnit);
-            std::vector<CanvasTriangle> triangles = convertModTriToTri(connections);
-            drawTriangles(window, triangles, colour);
+//            drawPoints(connections, window, colourUnit);
+            std::vector<std::tuple<Colour, CanvasTriangle>> triangles = convertModTriToTri(connections);
+            drawTriangles(window, triangles);
             for (ModelTriangle connection : connections) {
                 std::cout << connection << std::endl;
             }
-
-
-            std::cout << "DOWN" << std::endl;
         }
     } else if (event.type == SDL_MOUSEBUTTONDOWN) {
         window.savePPM("output.ppm");
